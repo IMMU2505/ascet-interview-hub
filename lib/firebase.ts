93d718544fb6fe9e35903af0873f292887f3bb1b
IMMU2505 <imdadullashaik123456@gmail.com>
2026-05-16 21:39:54 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,6 @@
-import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
+import { initializeApp, getApps } from 'firebase/app'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -10,5 +11,10 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 }
 
-const app = initializeApp(firebaseConfig)
+// Prevent re-initializing during hot reloads
+const app = getApps().length === 0? initializeApp(firebaseConfig) : getApps()[0]
+
 export const auth = getAuth(app)
+export const db = getFirestore(app)
+export const provider = new GoogleAuthProvider()
+export default app
