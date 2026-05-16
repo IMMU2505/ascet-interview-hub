a2d407f50fe47ec4c9592cddd069f9bbc9714398
IMMU2505 <imdadullashaik123456@gmail.com>
2026-05-16 14:32:58 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,5 @@
-import { initializeApp, getApps } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { initializeApp, getApps } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -12,4 +12,4 @@ const firebaseConfig = {
 
 const app = getApps().length === 0? initializeApp(firebaseConfig) : getApps()[0];
 export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
+export const provider = new GoogleAuthProvider();
